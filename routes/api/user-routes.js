369e052f31976159ddf7741324c6a1c3a2b70042
Dustin <dustin2400@hotmail.com>
2022-01-22 12:17:14 -0600

    testing ad friend route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const res = require('express/lib/response');
 const { User } = require('../../models');
 
 router.get('/', (req, res) => {
@@ -29,6 +30,50 @@ router.post('/', ({ body }, res) => {
     .catch(err => {
         res.status(400).json(err);
     });
+});
+
+router.put('/:id', ({ params, body }, res) => {
+    User.findOneAndUpdate( 
+        { _id: params.id },
+        body,
+        { new: true }
+    )
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({ message: 'No User found with this id!'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => res.status(400).json(err));
+});
+
+router.delete('/:id', ({ params }, res) => {
+    User.findOneAndDelete({ _id: params.id })
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id!'});
+            return;
+        }
+        res.json(dbUserData)
+    })
+    .catch(err => res.status(400).json(err));
+});
+
+router.post('/:userId/friends/:friendId', ({ params }, res) => {
+    User.findOneAndUpdate(
+        { _id: params.userId },
+        { $addToSet: { friends: params.friendId } },
+        { new: true }
+    )
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id!'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => res.json(err))
 });
 
 module.exports = router;
